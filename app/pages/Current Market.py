9dcb2199219f038d465af62c45e7cf31c0e0e898
jan463 <jangfeller@gmail.com>
2024-12-15 12:20:54 +0100

    updated

--- app/pages/Current Market.py
+++ app/pages/Current Market.py
@@ -7,7 +7,7 @@ st.title("Current Market")
 import streamlit as st
 
 # Assuming your image is saved as 'example.png' in the same directory as your script
-#st.image("current.png", caption="My Local PNG Image", use_column_width=True)
+st.image("current.png", caption="My Local PNG Image", use_column_width=True)
 
 
-st.components.v1.iframe(src="https://public.tableau.com/app/profile/jan.gfeller/viz/EDA_17341824351580/Treeboard", width=None, height=None, scrolling=False)
+#st.components.v1.iframe(src="https://public.tableau.com/app/profile/jan.gfeller/viz/EDA_17341824351580/Treeboard", width=None, height=None, scrolling=False)
